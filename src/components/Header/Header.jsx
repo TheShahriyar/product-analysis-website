95d7fbb1b6f5c5998eaec0b185accf038be531d3
TheShahriyar <shahriyar.31dec@gmail.com>
2022-12-21 13:13:54 +0600

    react setup complete

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h2>Header with Navigation</h2>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import Review from '../Review/Review';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>This is Homepage</h1>
+      <Banner />
+      <Review />
+    </div>
+  );
+};
+
+export default Home;
